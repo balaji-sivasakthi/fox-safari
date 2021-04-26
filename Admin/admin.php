c82915e721ba93bebdd02066af9202bfe4b103f1
balaji <balajisivasakthi7772@gmail.com>
2021-04-26 13:58:54 +0530

    test

--- Admin/admin.php
+++ Admin/admin.php
@@ -11,9 +11,12 @@
  <main>
 
     <div class="container">
+        <h1>Admin Panel</h1>
     <form action="<?php echo($_SERVER['PHP_SELF'])?>" method="post">
     
-
+            <input type="text" placeholder="User Name" name="user" id="user">
+            <input type="password" placeholder="Password" name="pass" id="pass">
+            <input type="submit" value="Login">
 
         </form>
     </div>
