93a1e2ab6a94135950bf8c74607abfafd8c5f04f
balaji-sivasakthi <balajisivasakthi2020@gmail.com>
2021-04-29 08:10:30 +0530

    backend in admin panel

---
+++ php/Admin/security.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+include('./Database/dbconfig.php');
+
+if($connection)
+{
+  // echo "Database Connected";
+}
+else
+{
+    header("Location: Database/dbconfig.php");
+}
+?>
